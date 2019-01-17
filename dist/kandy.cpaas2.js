b23e4f69f78f734089a6e2f8a3665e2c3c0e4c27
machine <janwa039@gmail.com>
2019-01-17 16:49:24 +0000

    49827

--- dist/kandy.cpaas2.js
+++ dist/kandy.cpaas2.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js (Next)
  * kandy.cpaas2.js
- * Version: 3.1.0-beta.49773
+ * Version: 3.1.0-beta.49814
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -29185,7 +29185,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '3.1.0-beta.49773';
+  let version = '3.1.0-beta.49814';
   log.info(`CPaaS SDK version: ${version}`);
 
   var sagas = [];
