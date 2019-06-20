cf60f2e7a7be0452ccd41d62e670aa6f3f70a87e
machine <janwa039@gmail.com>
2019-06-20 15:58:08 +0000

    39

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@kandy-io/cpaas-sdk@4.5.0/dist/kandy.js"
-  integrity="sha384-5VeOEWBzUu1bKvQ6EEwk/oN1+hcyXxl3ZKXxYKUVwaVoCot0o4wxf26ZznDLTJM6"
+  integrity="sha384-3NslRdrepxYLHgsUy/UF4GZ85VQpwm116GKxV4j5DTJWLzcfQ5w3W8nho9Y9Fe/w"
   crossorigin="anonymous">
 </script>
 ```
--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.cpaas.js
- * Version: 4.5.0-beta.38
+ * Version: 4.5.0-beta.39
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -30068,7 +30068,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '4.5.0-beta.38';
+  let version = '4.5.0-beta.39';
   log.info(`SDK version: ${version}`);
 
   var sagas = [];
