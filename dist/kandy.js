5daa42767b7bab9329df44c3f87eef1dda3f241b
machine <janwa039@gmail.com>
2020-09-23 20:45:14 +0000

    538

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.cpaas.js
- * Version: 4.20.0-beta.537
+ * Version: 4.20.0-beta.538
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -31956,7 +31956,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '4.20.0-beta.537';
+  return '4.20.0-beta.538';
 }
 
 /***/ }),
