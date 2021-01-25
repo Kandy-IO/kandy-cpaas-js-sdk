780d23cabe85e7f1d7e26c59ec2db581fa039ea3
machine <janwa039@gmail.com>
2021-01-25 22:03:47 +0000

    606

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To install a particular build branch or beta branch with the Github URL, run:
 ### In the browser :
 ```html
 <script src="https://unpkg.com/@kandy-io/cpaas-sdk@4.24.0/dist/kandy.js"
-  integrity="sha384-JalOmnGICjLyqzh0CpCIWTjQkeIrhHLaKVo+GCectOlWJ4crAoCLq/CQFI5tCEce"
+  integrity="sha384-N50P+feI9JcZA/8RYaanaPpMSsNZF6oB2KywA/EdBCuo3qkwgT4QaGYbDKum3ZXI"
   crossorigin="anonymous">
 </script>
 ```
--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.cpaas.js
- * Version: 4.24.0-beta.605
+ * Version: 4.24.0-beta.606
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -32256,7 +32256,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '4.24.0-beta.605';
+  return '4.24.0-beta.606';
 }
 
 /***/ }),
