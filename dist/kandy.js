129de0aeebe2a8881e79d919efa5d016c03b8d3b
machine <janwa039@gmail.com>
2021-04-01 19:59:17 +0000

    643

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.cpaas.js
- * Version: 4.26.0-beta.642
+ * Version: 4.26.0-beta.643
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -32180,7 +32180,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '4.26.0-beta.642';
+  return '4.26.0-beta.643';
 }
 
 /***/ }),
