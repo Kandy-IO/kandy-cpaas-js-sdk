783a12e2deb033d078458082452c5a51bcb8c34c
machine <janwa039@gmail.com>
2022-09-27 19:22:44 +0000

    936

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.cpaas.js
- * Version: 5.2.0-beta.935
+ * Version: 5.2.0-beta.936
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -6972,7 +6972,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '5.2.0-beta.935';
+  return '5.2.0-beta.936';
 }
 
 /***/ }),
